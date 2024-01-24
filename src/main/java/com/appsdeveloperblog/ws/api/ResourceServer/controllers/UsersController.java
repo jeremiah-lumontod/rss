23d7cf281ca6a1b45087c0fc09e43b0cda3320a6
Jerry Lumontod <>
2024-01-24 14:33:57 +0800

    @Secured("ROLE_developer")

--- src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
@@ -1,8 +1,7 @@
 package com.appsdeveloperblog.ws.api.ResourceServer.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -12,6 +11,11 @@ public class UsersController {
 	public String status() {
 		return "Working...";
 	}
-	
-	
+
+	@Secured("ROLE_developer")
+	@DeleteMapping(path = "/{id}")
+	public String deleteUser(@PathVariable String id) {
+		return "Deleted user with id " + id;
+	}
+
 }
