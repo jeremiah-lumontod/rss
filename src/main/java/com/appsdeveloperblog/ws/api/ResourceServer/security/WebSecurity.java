34a5777ce8c86333b5f582ad227098471afbea72
Jerry Lumontod <>
2024-01-24 14:22:06 +0800

    @EnableGlobalMethodSecurity(securedEnabled=true)

--- src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableGlobalMethodSecurity
+@EnableGlobalMethodSecurity(securedEnabled=true)
 @EnableWebSecurity
 public class WebSecurity {
 	
