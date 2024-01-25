88bfcc3c14bc7fa664addf4901cb371555cc375b
Jerry Lumontod <>
2024-01-25 11:07:55 +0800

    @PostAuthorize("returnObject.userId == #jwt.subject")

--- src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
@@ -1,10 +1,13 @@
 package com.appsdeveloperblog.ws.api.ResourceServer.controllers;
 
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
+import com.appsdeveloperblog.ws.api.ResourceServer.response.UserRest;
+
 
 @RestController
 @RequestMapping("/users")
@@ -21,6 +24,12 @@ public class UsersController {
 	@DeleteMapping(path = "/{id}")
 	public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
 		return "Deleted user with id " + id + " and JWT subject " + jwt.getSubject();
+	}
+
+	@PostAuthorize("returnObject.userId == #jwt.subject")
+	@GetMapping(path = "/{id}")
+	public UserRest getUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+		return new UserRest("jml", "dev",id);
 	}
 
 }
